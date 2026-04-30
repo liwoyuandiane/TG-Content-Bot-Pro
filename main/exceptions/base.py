b84d5e7ef0ad51ac94f635dfd7a075cafb3ff9b2
liwoyuandiane <liwoyuandiane@gmail.com>
2026-04-30 11:44:47 +0800

    修复BaseBotException缺少__init__的问题

--- main/exceptions/base.py
+++ main/exceptions/base.py
@@ -3,4 +3,8 @@
 
 class BaseBotException(Exception):
     """基础异常类"""
-    pass
+    
+    def __init__(self, message: str, code: str = "UNKNOWN_ERROR") -> None:
+        super().__init__(message)
+        self.message = message
+        self.code = code
